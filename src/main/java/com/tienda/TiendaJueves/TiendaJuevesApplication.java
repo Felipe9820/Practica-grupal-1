4ef4f50443328258b5a17f404ee335a59bed041b
YostinArias <yostinarias220@gmail.com>
2023-02-09 17:59:24 -0600

    Commit final

---
+++ src/main/java/com/tienda/TiendaJueves/TiendaJuevesApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.TiendaJueves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJuevesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJuevesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda/TiendaJueves/TiendaJuevesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.TiendaJueves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaJuevesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
